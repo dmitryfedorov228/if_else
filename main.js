f1e3f2790842035edd0e9997e7f83358cabe4cff
dmitryfedorov228 <116007725+dmitryfedorov228@users.noreply.github.com>
2023-03-23 19:03:59 +0600

    add files

---
+++ main.js
@@ -0,0 +1,114 @@
+// ! task1
+
+// let year = 2023
+
+// if( year > 2015  ) {
+//     alert(year)
+// }
+
+// ! task2
+
+// let a = 10
+// let b = 10
+
+// if (a - b === 0) {
+//     alert('будет 0')
+// }
+// else{
+//     alert('Ошибка')
+// }
+
+// ! task3 
+
+// let a = prompt('Какое «официальное» название JavaScript?')
+
+// if (a === 'ECMAScript') {
+//     alert('Верно!')
+// } else{
+//     alert('Не знаете? ECMAScript!')
+// }
+
+// ! task4
+
+// let a = +prompt('Введите число');
+// let b = +prompt('Введите число');
+// if (a + b === 10) {
+//   alert(a + b);
+// } else {
+//   console.log(a + b);
+// }
+
+// ! task 5
+
+// let a = confirm('Вам есть 18?')
+
+// if ( a === true) {
+//     alert('Добро пожаловать')
+// }
+// else{
+//     alert('Вход запрещен!')
+// }
+
+// ! task 6
+
+// const str = 'Javascrip' 
+// const result = str === 'Javascript' ? console.log("Frontend") : console.log("Backend");
+
+// ! Extra tasks 
+
+// ? 1
+// let num = +prompt('Введите порядковый номер времени года');
+
+//  if(num === 1) {
+//     alert('Осень')
+//  }
+//  else if(num === 2) {
+//     alert('Зима')
+//  }
+//  else if(num === 3) {
+//     alert('Весна')
+//  }
+//  else if(num === 4) {
+//     alert('Лето')
+//  }
+//  else{
+//     alert('Нет такого номера')
+//  }
+
+// ? 2
+
+// let a = prompt('Как тебя зовут?')
+
+
+// if (a === null) {
+//     alert ('Вы отменили действие')
+// }
+// else if ( a === a ) {
+//     confirm('Вы уверены?')
+// } 
+// else if ( a === a) {
+//     alert('Ваше имя ', a)
+// } 
+// else{   
+//     alert('Попробуйте вспомнить')
+// }
+
+// ? 3
+
+
+    // let min = +prompt ("Введите число от 0 до 59");
+    //   if (min >= 0 && min <= 14) {
+    //     console.log ("Первая");
+    //  }
+    //   else if (min >= 15 && min <= 30) {
+    //     console.log ("Вторая");
+    //  }
+    //  else if (min >= 31 && min <= 45) {
+    //     console.log ("Третья");
+    //  }
+    //   else if (min >= 46 && min <= 59) {
+    //     console.log ("Четвертая");
+    //  }
+    //  else{
+    //     alert('Дурак до 59!!!!')
+    //  }
